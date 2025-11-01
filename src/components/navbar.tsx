9b19a8e6cbe109e7531d7259bb60cc53337f4f9e
ariful97768 <ariful97768@gmail.com>
2025-11-01 18:56:14 +0600

    added static navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { CgShoppingBag } from "react-icons/cg";
+import { FaRegHeart, FaRegUser } from "react-icons/fa";
+// import { GiHamburgerMenu } from "react-icons/gi";
+
+export default function Navbar() {
+  return (
+    <nav className="flex mt-7 justify-between items-center max-w-7xl mx-auto">
+      <div className="flex gap-9 items-center">
+        {/* <GiHamburgerMenu size={20} /> */}
+        <ul className="flex gap-8 font-semibold items-center">
+          <li>
+            <Link href={"#"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"#"}>Collections</Link>
+          </li>
+          <li>
+            <Link href={"#"}>New</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1 className="text-4xl font-black">Stylofy</h1>
+      </div>
+      <div className="flex gap-11 items-center">
+        <span className="p-3 rounded-full -rotate-45 bg-black text-white">
+          <FaRegHeart size={16} />
+        </span>
+        <div className="flex items-center">
+          <span className="text-xs px-5 py-3.5 hover:cursor-default bg-black text-white rounded-[18px]">
+            Cart
+          </span>
+          <div className="rounded-full relative border-6 p-2 -ml-1">
+            <CgShoppingBag size={16} />
+            {/* <div className="absolute -top-1 -right-5 p-px  bg-[#f2f2f2f2] text-xs rounded-full">
+              <div className="flex items-center w-3.5 h-3.5 justify-center shrink-0">1</div>
+            </div> */}
+          </div>
+        </div>
+        <span className="p-3.5 bg-black text-white rounded-full">
+          <FaRegUser size={16} />
+        </span>
+      </div>
+    </nav>
+  );
+}

5277f1d23d86fd0a06ca01d2ebbd6bc513c29ad0
ariful97768 <ariful97768@gmail.com>
2025-11-03 16:26:07 +0600

    added log in page, added shadcn login form, wrapped layout for login and register route with context provider

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+'use client'
+import AuthProvider from "@/Context/AuthProvider";
+import { ReactNode } from "react";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return <AuthProvider>{children}</AuthProvider>;
+}

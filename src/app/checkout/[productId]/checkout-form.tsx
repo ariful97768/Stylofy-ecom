6bf3704be2a06e7e1f9d5bd5277180d4e386561d
ariful97768 <ariful97768@gmail.com>
2025-11-05 16:34:01 +0600

    added checkout form and pages functionality

---
+++ src/app/checkout/[productId]/checkout-form.tsx
@@ -0,0 +1,112 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Product } from "@/components/ui/product-card";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { auth } from "@/firebase.config";
+import { FormEvent, useState } from "react";
+import { toast } from "sonner";
+
+export default function CheckoutForm({ product }: { product: Product }) {
+  const [loading, setLoading] = useState(false);
+
+  async function confirmOrder(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.currentTarget;
+    const formFields = new FormData(form);
+    const data = Object.fromEntries(formFields);
+    try {
+      const orderInfo = {
+        user: auth.currentUser?.email,
+        product: product._id,
+        discount: product.discount,
+        quantity: 1,
+        price: (
+          product.price -
+          (product.price / 100) * product.discount
+        ).toFixed(2),
+        shipping: 70,
+        userInfo: {
+          name: data.name,
+          email: data.email,
+        },
+        order: {
+          country: data.country,
+          city: data.city,
+          address: data.address,
+        },
+      };
+
+      const res = await toast
+        .promise(
+          fetch("http://localhost:5000/confirm-order", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(orderInfo),
+          }),
+          {
+            loading: "Submitting order...",
+            success: () => "Order submitted",
+            error: (a) => {
+              return `${a}`;
+            },
+          }
+        )
+        .unwrap();
+      form.reset();
+    } catch (error) {
+      toast.error("Could not confirm the order");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <form onSubmit={(e) => confirmOrder(e)}>
+      <h3 className="uppercase text-sm font-medium mb-5">Shipping address</h3>
+      <div className="space-y-4">
+        <Input required name="name" placeholder="Name" type="text" />
+        <Input required name="email" placeholder="Email" type="email" />
+        <Select required name="country">
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Country" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Bangladesh">Bangladesh</SelectItem>
+            <SelectItem value="Bidesh">Bidesh</SelectItem>
+            <SelectItem value="Shondesh">Shondesh</SelectItem>
+          </SelectContent>
+        </Select>
+        <Input required name="city" placeholder="City" type="text" />
+        <Input required name="address" placeholder="Address" type="text" />
+        <RadioGroup name="payment" required defaultValue="cod" className="flex">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="cod" id="cod" />
+            <Label htmlFor="cod">Cash On Delivery</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="card" id="card" />
+            <Label htmlFor="card">Card</Label>
+          </div>
+        </RadioGroup>
+        <button
+          disabled={loading}
+          className="py-2.5 mt-10 min:w-[100px] max-h-max w-full items-center justify-center flex px-15 bg-black/30 rounded-none uppercase text-sm font-semibold shrink-0"
+        >
+          Confirm Order
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/app/checkout/[productId]/page.tsx
+++ src/app/checkout/[productId]/page.tsx
@@ -1,18 +1,28 @@
-import { GiCancel } from "react-icons/gi";
-import img from "@/assets/feature-product.png";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import Image from "next/image";
+import { cookies } from "next/headers";
+import { type Product } from "@/components/ui/product-card";
+import CheckoutForm from "./checkout-form";
 
-export default function Checkout() {
+export default async function Checkout({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const cookiesStore = await cookies();
+  const token = cookiesStore.get("token")?.value;
+
+  const id = (await params).productId;
+  const res = await fetch(`http://localhost:5000/get-product/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      cookie: `token=${token}`,
+    },
+    credentials: "include",
+  });
+
+  const product: Product = await res.json();
+
   return (
     <main className="max-w-7xl tracking-wider mx-auto mt-14">
       <div>
@@ -21,40 +31,7 @@ export default function Checkout() {
       </div>
       <div className="flex justify-between">
         <div className="max-w-[450px] mt-8">
-          <div>
-            <h3 className="uppercase text-sm font-medium mb-5">
-              Shipping address
-            </h3>
-            <div className="space-y-4">
-              <Input placeholder="Name" type="text" />
-              <Input placeholder="Email" type="email" />
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Country" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="Bangladesh">Bangladesh</SelectItem>
-                  <SelectItem value="Bidesh">Bidesh</SelectItem>
-                  <SelectItem value="Shondesh">Shondesh</SelectItem>
-                </SelectContent>
-              </Select>
-              <Input placeholder="City" type="text" />
-              <Input placeholder="Address" type="text" />
-              <RadioGroup defaultValue="cod" className="flex">
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="cod" id="cod" />
-                  <Label htmlFor="cod">Cash On Delivery</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="card" id="card" />
-                  <Label htmlFor="card">Card</Label>
-                </div>
-              </RadioGroup>
-              <button className="py-2.5 mt-10 min:w-[100px] max-h-max w-full items-center justify-center flex px-15 bg-black/30 rounded-none uppercase text-sm font-semibold shrink-0">
-                Confirm Order
-              </button>
-            </div>
-          </div>
+          <CheckoutForm product={product} />
         </div>
         <div className="mr-20">
           <div className="border w-[406px] space-y-8 p-8 pb-6">
@@ -62,41 +39,36 @@ export default function Checkout() {
             <div className="w-full space-y-6">
               <div id="cart-item" className="flex w-full gap-3">
                 <div className="relative border shrink-0 border-gray-400 h-33 w-28">
-                  <Image src={img} fill className="object-cover" alt="" />
+                  <Image
+                    src={product.images[0]}
+                    unoptimized
+                    fill
+                    className="object-cover"
+                    alt="product image"
+                  />
                 </div>
                 <div className="flex py-4 flex-col w-full">
                   <div className="grow">
                     <div className="flex w-full justify-between items-center">
-                      <h4 className="text-sm">Basic Heavy T-Shirt</h4>
-                      <span>
+                      <h4 className="text-sm">{product.name}</h4>
+                      {/* <span>
                         <GiCancel size={16} className="text-gray-700" />
-                      </span>
+                      </span> */}
                     </div>
-                    <span className="text-xs">XL</span>
+                    <span className="text-xs">{product?.size}</span>
                   </div>
                   <div className="flex justify-between text-xs">
                     <span>(1)</span>
-                    <span>$99</span>
-                  </div>
-                </div>
-              </div>
-              <div id="cart-item" className="flex w-full gap-3">
-                <div className="relative border shrink-0 border-gray-400 h-33 w-28">
-                  <Image src={img} fill className="object-cover" alt="" />
-                </div>
-                <div className="flex py-4 flex-col w-full">
-                  <div className="grow">
-                    <div className="flex w-full justify-between items-center">
-                      <h4 className="text-sm">Basic Heavy T-Shirt</h4>
+                    <div className="flex items-center gap-3">
+                      <span className="line-through">${product.price}</span>
                       <span>
-                        <GiCancel size={16} className="text-gray-700" />
+                        $
+                        {(
+                          product.price -
+                          (product.price / 100) * product.discount
+                        ).toFixed(2)}
                       </span>
                     </div>
-                    <span className="text-xs">XL</span>
-                  </div>
-                  <div className="flex justify-between text-xs">
-                    <span>(1)</span>
-                    <span>$99</span>
                   </div>
                 </div>
               </div>
@@ -105,7 +77,13 @@ export default function Checkout() {
               <div className="space-y-2 border-y border-gray-300 py-3.5">
                 <div className="flex justify-between text-xs">
                   <h4>Subtotal</h4>
-                  <span>$180</span>
+                  <span>
+                    $
+                    {(
+                      product.price -
+                      (product.price / 100) * product.discount
+                    ).toFixed(2)}
+                  </span>
                 </div>
                 <div className="flex justify-between text-xs">
                   <h4>Shipping</h4>
@@ -114,7 +92,14 @@ export default function Checkout() {
               </div>
               <div className="flex justify-between text-sm">
                 <h4>Total</h4>
-                <span>$180</span>
+                <span>
+                  $
+                  {(
+                    product.price -
+                    (product.price / 100) * product.discount +
+                    70
+                  ).toFixed(2)}
+                </span>
               </div>
             </div>
           </div>

76ce9f0b689aa8b474e4697d8d3c8220008e2cca
ariful97768 <ariful97768@gmail.com>
2025-11-05 16:37:26 +0600

    updated product card component for dynamic ui

--- src/components/ui/product-card.tsx
+++ src/components/ui/product-card.tsx
@@ -1,21 +1,41 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
-
-export default function ProductCard({ img }: { img: string | StaticImport }) {
+import Link from "next/link";
+export interface Product {
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  images: string[];
+  size: string;
+  discount: number;
+  quantity: number;
+  createdAt: string;
+  updatedAt: string;
+  _id: string;
+}
+export default function ProductCard({ product }: { product: Product }) {
   return (
-    <div id="product-card" className="max-w-max">
-      <div className="relative border border-gray-400 w-[301px] h-[308px]">
-        <Image src={img} fill className="object-cover" alt="product" />
-      </div>
-      <div className="pt-3.5">
-        <span className="text-black/60 text-xs font-medium">
-          V-Neck T-Shirt
-        </span>
-        <div className="flex items-center justify-between">
-          <h2>Embroidered Seersucker Shirt</h2>
-          <span>$99</span>
+    <div id="product-card" className="max-w-max border">
+      <Link href={`/products/${product._id}`}>
+        <div className="relative border border-gray-400 w-[301px] h-[308px]">
+          <Image
+            unoptimized
+            src={product.images[0]}
+            fill
+            className="object-cover"
+            alt="product"
+          />
         </div>
-      </div>
+        <div className="pt-3.5">
+          <span className="text-black/60 text-xs font-medium">
+            {product.category}
+          </span>
+          <div className="flex  items-center justify-between">
+            <h2 className="w-65 truncate">{product.name}</h2>
+            <span>${product.price}</span>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
